6e06424c47ca3397b54037ddd898323d479134ba
Aznpepsi <Jamesbanh@yahoo.com>
2020-11-04 19:13:23 -0800

    edited

--- spacetime-crawler4py-master/extract.py
+++ spacetime-crawler4py-master/extract.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 
 def unique(url_text_file):
     print("f")
@@ -47,7 +48,7 @@ def sort_URLS(url_text_file):
     file.close()
 
 def get_50_most(words_file):
-    frequencies = dict()
+    frequencies = defaultdict(int)
     output_file = open("number_3.txt", "a", encoding = "utf-8")
     stop_words = open("stopwords.txt", "a", encoding = "utf-8")
     stop_list = []
@@ -55,14 +56,12 @@ def get_50_most(words_file):
         line = line.rstrip() 
         stop_list += line.split()
     with open(words_file, 'a', encoding='utf8') as words_file:
-        for word in words_file: #add a condition where the it will not add the word into the dictionary if it is a stop word. 
-            if word not in stop_list:
-                if word not in frequencies:
-                    frequencies[word] = 1
-                else:
+        for line in words_file: #add a condition where the it will not add the word into the dictionary if it is a stop word. 
+            for word in list(line):
+                if word not in stop_list:
                     frequencies[word] += 1 #creates the dict of frequencies in the words file
     counter = 0
-    for (word,frequency) in sorted(frequencies, key = lambda x: -x[1]): #loops through the sorted dictionary where the largest frequencies are in the front
+    for (word,frequency) in sorted(frequencies.items(), key = lambda x: -x[1]): #loops through the sorted dictionary where the largest frequencies are in the front
         if counter < 50: #counter to make sure it doesnt go over 50
             output_file.write(word + "-->" + frequency + "/n")
             counter += 1
@@ -76,3 +75,5 @@ def get_50_most(words_file):
 if __name__ == "__main__":
     unique("URLs.txt")
     longest("longest_page.txt")
+    sort_URLS("URLs.txt")
+    get_50_most("content.txt")
