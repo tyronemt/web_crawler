3d30bef014cf14801ca0e37aa028d9793fafe472
unknown <tyrone.tong800@gmail.com>
2020-11-05 16:31:27 -0800

    fixed code

--- spacetime-crawler4py-master/scraper.py
+++ spacetime-crawler4py-master/scraper.py
@@ -10,7 +10,7 @@ valid_netloc = ["ics.uci.edu","cs.uci.edu","stat.uci.edu","informatics.uci.edu"]
 skip = ["archive.uci.edu", "intranet.ics.uci.edu", "grape.ics.uci.edu", "evoke.ics.uci.edu", "ganglia.ics.uci.edu", "cbcl.ics.uci.edu"]
 
 no_list =["calendar","events","img","apk", "jpg","css","js","bmp","pptx","doc","docx","xls","data","dat","gif","gz","svg","txt","py","rkt","json","pdf","jpeg","ico","png",
-            "mp2","mp3","mp4","wav","avi","mov","pdf","ps","eps","tex","ppt","exe", "odc",
+            "mp2","mp3","mp4","wav","avi","mov","pdf",".ps","eps","tex","ppt","exe", "odc","mpg",".nb", ".java", ".bib", "xml","scm",
             "tar","msi","bin","psd","dmg","epub","jar","csv","zip","rar","wp-content"]
 
 
@@ -145,7 +145,7 @@ def check_netloc(parsed_url):
     
     if "/department/information_computer_sciences" in parsed_url.path:
         return True
-    elif (netloc == "hack.ics.uci.edu" and "gallery" in parsed_url.path) or (netloc == "ics.uci.edu" and "publications" in parsed_url.path):
+    elif (netloc == "hack.ics.uci.edu" and "gallery" in parsed_url.path) or (netloc == "ics.uci.edu" and "publications" in parsed_url.path) or (netloc == "ics.uci.edu" and "~yamingy" in parsed_url.path):
         return False
     
     for i in skip:
